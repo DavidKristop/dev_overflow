bc8aee230124d8ebb09dfe2f913d9fbeeeeb167c
DavidKristop <krondavid64@gmail.com>
2026-05-09 10:49:33 +0700

    Add website theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
     return (
         <div>
-            <h1 className="text-3xl">Welcome to the Ultimate Next.js course!</h1>
+            <h1 className="text-3xl text-light-500 font-black">Welcome to the Ultimate Next.js course!</h1>
         </div>
     )
 }
