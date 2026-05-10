e2368f73736565f75acf562251f4413c21c1b347
DavidKristop <krondavid64@gmail.com>
2026-05-10 13:30:56 +0700

    Setup Auth Layout, Social Auth Form, Next Auth, OAuth with Github and Google Providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,40 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
 import "./globals.css";
+import { auth } from "@/auth";
 import ThemeProvider from "@/context/Theme";
-import theme from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "@/components/ui/sonner"; // Ensure this path is correct
 
 export const metadata: Metadata = {
-  title: "Dev Overflow",
-  description:
-      "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-  icons: {
-    icon: "/images/site-logo.svg",
-  },
+    title: "DevOverflow",
+    description: "A community-driven platform for asking and answering programming questions.",
+    icons: {
+        icon: "/images/site-logo.svg",
+    },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning
-    >
-      <body className="min-h-full flex flex-col">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+    const session = await auth();
+
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className="min-h-screen bg-background font-sans antialiased">
+        <SessionProvider session={session}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+                <Toaster />
+            </ThemeProvider>
+        </SessionProvider>
+        </body>
+        </html>
+    );
+};
+
+export default RootLayout;
