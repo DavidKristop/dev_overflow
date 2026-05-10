56db39c45818922c36fb4586c97eecaa44d250ad
DavidKristop <krondavid64@gmail.com>
2026-05-10 16:21:13 +0700

    Setup AuthForm for user Sign-In and Sign-Up options

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import {SignInSchema} from "@/lib/validations";
+
 const SignIn = () => {
     return (
-        <div>Sign In</div>
+        <AuthForm
+            schema={SignInSchema}
+            defaultValues={{
+                email: '',
+                password: ''
+            }}
+            onSubmit={(data) => Promise.resolve({ success: true, data })}
+            formType="SIGN_IN"
+        />
     )
 }
 export default SignIn
